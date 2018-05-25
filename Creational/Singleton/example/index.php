b988c977c879b23a4f4445fabd7730dddb7b3eb0
Alessandro Minoccheri <alessandro@studiomado.it>
2018-05-25 10:20:25 +0200

    moved singleton file into specific path

--- Creational/Singleton/example/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$preferences = Preferences::getInstance();
-$preferences->setProperty('foo', 'bar');
-
-unset($preferences);
-
-$preferences2 = Preferences::getInstance();
-echo $preferences2->getProperty('foo'); //value is not lost and output is: bar
--- src/Singleton/Preferences.php
+++ src/Singleton/Preferences.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Singleton;
+
 class Preferences
 {
     private $properties = [];
